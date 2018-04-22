7fa9e93422d94123ef7e85f571a22959cf44c1e2
JC <jc@lifove.net>
2018-04-22 20:25:02 +0900

    Update demonstrator

--- src/main/java/edu/handong/csee/java/example/keyboard/characters/TreeDemonstrator.java
+++ src/main/java/edu/handong/csee/java/example/keyboard/characters/TreeDemonstrator.java
@@ -2,17 +2,19 @@ package edu.handong.csee.java.example.keyboard.characters;
 
 public class TreeDemonstrator {
 
-	public static final int INDENT = 5;
+	public final int INDENT = 5;
 	public static final int TREE_TOP_WIDTH = 21;// must be odd
 	public static final int TREE_BOTTOM_WIDTH = 4;
 	public static final int TREE_BOTTOM_HEIGHT = 4;
 
 	public static void main(String[] args) {
-		drawTree(TREE_TOP_WIDTH, TREE_BOTTOM_WIDTH,
+		
+		TreeDemonstrator demonstrator = new TreeDemonstrator();
+		demonstrator.drawTree(TREE_TOP_WIDTH, TREE_BOTTOM_WIDTH,
 				TREE_BOTTOM_HEIGHT);
 	}
 
-	public static void drawTree(int topWidth, int bottomWidth, int bottomHeight)
+	public void drawTree(int topWidth, int bottomWidth, int bottomHeight)
 	{
 		System.out.println(" Save the Redwoods!");
 		TriangleInterface treeTop = new Triangle(INDENT, topWidth);
@@ -23,11 +25,11 @@ public class TreeDemonstrator {
 		drawTrunk(treeTrunk);
 	}
 
-	private static void drawTop(TriangleInterface treeTop) {
+	private void drawTop(TriangleInterface treeTop) {
 		treeTop.drawAt(1);
 	}
 
-	private static void drawTrunk(RectangleInterface treeTrunk) {
+	private void drawTrunk(RectangleInterface treeTrunk) {
 		treeTrunk.drawHere(); // or treeTrunk.drawAt(0); }
 
 	}
